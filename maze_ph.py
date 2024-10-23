065a9e1e6860d6da3abcb8d5fbe9244597241d93
Peter Harrison <peter.harrison@helicron.net>
2024-10-23 22:52:53 +0100

    replace strings with numbers for directions in maze_ph.py (no improvement in performance)

--- maze_ph.py
+++ maze_ph.py
@@ -17,35 +17,35 @@ class Maze:
         self.cost = [[None for _ in range(
             self.size)] for _ in range(self.size)]
         self.walls = {
-            (row, col): {"N": WALL_UNKNOWN, "E": WALL_UNKNOWN, "S": WALL_UNKNOWN, "W": WALL_UNKNOWN}
+            (row, col): {DIR_NORTH: WALL_UNKNOWN, DIR_EAST: WALL_UNKNOWN, DIR_SOUTH: WALL_UNKNOWN, DIR_WEST: WALL_UNKNOWN}
             for row in range(size)
             for col in range(size)
         }
 
     def clear_walls(self):
         self.walls = {
-            (row, col): {"N": WALL_UNKNOWN, "E": WALL_UNKNOWN, "S": WALL_UNKNOWN, "W": WALL_UNKNOWN}
+            (row, col): {DIR_NORTH: WALL_UNKNOWN, DIR_EAST: WALL_UNKNOWN, DIR_SOUTH: WALL_UNKNOWN, DIR_WEST: WALL_UNKNOWN}
             for row in range(self.size)
             for col in range(self.size)
         }
         for cell in range(16):
-            maze.set_wall(cell, 0, "S", WALL_PRESENT)
-            maze.set_wall(cell, 15, "N", WALL_PRESENT)
-            maze.set_wall(0, cell, "W",  WALL_PRESENT)
-            maze.set_wall(15, cell, "E", WALL_PRESENT)
+            maze.set_wall(cell, 0, DIR_SOUTH, WALL_PRESENT)
+            maze.set_wall(cell, 15, DIR_NORTH, WALL_PRESENT)
+            maze.set_wall(0, cell, DIR_WEST,  WALL_PRESENT)
+            maze.set_wall(15, cell, DIR_EAST, WALL_PRESENT)
 
     def set_wall(self, col, row, direction, state):
         if (col, row) in self.walls and direction in self.walls[(col, row)]:
             self.walls[(col, row)][direction] = state
             # Add wall on the opposite side for the neighboring cell
-            if direction == "N" and row < self.size - 1:
-                self.walls[(col, row + 1)]["S"] = state
-            elif direction == "E" and col < self.size - 1:
-                self.walls[(col + 1, row)]["W"] = state
-            elif direction == "S" and row > 0:
-                self.walls[(col, row - 1)]["N"] = state
-            elif direction == "W" and col > 0:
-                self.walls[(col - 1, row)]["E"] = state
+            if direction == DIR_NORTH and row < self.size - 1:
+                self.walls[(col, row + 1)][DIR_SOUTH] = state
+            elif direction == DIR_EAST and col < self.size - 1:
+                self.walls[(col + 1, row)][DIR_WEST] = state
+            elif direction == DIR_SOUTH and row > 0:
+                self.walls[(col, row - 1)][DIR_NORTH] = state
+            elif direction == DIR_WEST and col > 0:
+                self.walls[(col - 1, row)][DIR_EAST] = state
 
     def get_wall(self, col, row, direction):
         if (col, row) in self.walls and direction in self.walls[(col, row)]:
@@ -61,14 +61,14 @@ class Maze:
         for row in range(self.size - 1, -1, -1):
             line = "+"
             for col in range(self.size):
-                if self.get_wall(col, row, "N") == WALL_PRESENT:
+                if self.get_wall(col, row, DIR_NORTH) == WALL_PRESENT:
                     line += "---+"
                 else:
                     line += "   +"
             print(line)
             line = ""
             for col in range(self.size):
-                if self.get_wall(col, row, "W") == WALL_PRESENT:
+                if self.get_wall(col, row, DIR_WEST) == WALL_PRESENT:
                     line += "|"
                 else:
                     line += " "
@@ -76,14 +76,14 @@ class Maze:
                     line += f"{self.cost[col][row]:>3}"
                 else:
                     line += "   "
-            if self.get_wall(15, row, "E") == WALL_PRESENT:
+            if self.get_wall(15, row, DIR_EAST) == WALL_PRESENT:
                 line += "|"  # Rightmost boundary
             else:
                 line += " "
             print(line)
         line = "+"
         for col in range(self.size):
-            if self.get_wall(col, 0, "S") == WALL_PRESENT:
+            if self.get_wall(col, 0, DIR_SOUTH) == WALL_PRESENT:
                 line += "---+"
             else:
                 line += "   +"
@@ -104,25 +104,25 @@ class Maze:
             dist = dist + 1
             if (head >= QUEUE_LENGTH):
                 head = 0
-            if not self.has_wall(x, y, "N") and self.cost[x][y+1] == MAX_COST:
+            if not self.has_wall(x, y, DIR_NORTH) and self.cost[x][y+1] == MAX_COST:
                 queue[tail] = (x, y + 1, dist)
                 self.cost[x][y+1] = dist
                 tail += 1
                 if (tail >= QUEUE_LENGTH):
                     tail = 0
-            if not self.has_wall(x, y, "E") and self.cost[x + 1][y] == MAX_COST:
+            if not self.has_wall(x, y, DIR_EAST) and self.cost[x + 1][y] == MAX_COST:
                 self.cost[x + 1][y] = dist
                 queue[tail] = (x + 1, y, dist)
                 tail += 1
                 if (tail >= QUEUE_LENGTH):
                     tail = 0
-            if not self.has_wall(x, y, "S") and self.cost[x][y - 1] == MAX_COST:
+            if not self.has_wall(x, y, DIR_SOUTH) and self.cost[x][y - 1] == MAX_COST:
                 self.cost[x][y - 1] = dist
                 queue[tail] = (x, y - 1, dist)
                 tail += 1
                 if (tail >= QUEUE_LENGTH):
                     tail = 0
-            if not self.has_wall(x, y, "W") and self.cost[x - 1][y] == MAX_COST:
+            if not self.has_wall(x, y, DIR_WEST) and self.cost[x - 1][y] == MAX_COST:
                 self.cost[x - 1][y] = dist
                 queue[tail] = (x - 1, y, dist)
                 tail += 1
@@ -137,19 +137,19 @@ if __name__ == "__main__":
     maze = Maze()
 
     maze.clear_walls()
-    maze.set_wall(7, 7, "W", WALL_PRESENT)
-    maze.set_wall(7, 7, "S", WALL_PRESENT)
-    maze.set_wall(8, 7, "S", WALL_PRESENT)
-    maze.set_wall(8, 7, "E", WALL_PRESENT)
-    maze.set_wall(8, 8, "E", WALL_ABSENT)
-    maze.set_wall(8, 8, "N", WALL_PRESENT)
-    maze.set_wall(7, 8, "N", WALL_PRESENT)
-    maze.set_wall(7, 8, "W", WALL_PRESENT)
+    maze.set_wall(7, 7, DIR_WEST, WALL_PRESENT)
+    maze.set_wall(7, 7, DIR_SOUTH, WALL_PRESENT)
+    maze.set_wall(8, 7, DIR_SOUTH, WALL_PRESENT)
+    maze.set_wall(8, 7, DIR_EAST, WALL_PRESENT)
+    maze.set_wall(8, 8, DIR_EAST, WALL_ABSENT)
+    maze.set_wall(8, 8, DIR_NORTH, WALL_PRESENT)
+    maze.set_wall(7, 8, DIR_NORTH, WALL_PRESENT)
+    maze.set_wall(7, 8, DIR_WEST, WALL_PRESENT)
     # check that a closed off area does not get flooded
-    maze.set_wall(4, 4, "N", WALL_PRESENT)
-    maze.set_wall(4, 4, "E", WALL_PRESENT)
-    maze.set_wall(4, 4, "S", WALL_PRESENT)
-    maze.set_wall(4, 4, "W", WALL_PRESENT)
+    maze.set_wall(4, 4, DIR_NORTH, WALL_PRESENT)
+    maze.set_wall(4, 4, DIR_EAST, WALL_PRESENT)
+    maze.set_wall(4, 4, DIR_SOUTH, WALL_PRESENT)
+    maze.set_wall(4, 4, DIR_WEST, WALL_PRESENT)
 
     maze.print_maze()
 
