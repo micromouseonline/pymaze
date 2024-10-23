908ce3955cf1a263bd654d3df8e2b1b8b526562c
micromouseonline <peter.harrison@helicron.net>
2024-10-23 13:13:38 +0100

    list testsdetect platform

--- maze_tests.py
+++ maze_tests.py
@@ -1,9 +1,19 @@
 """
  Here can go tests for the maze implementations
 """
+import sys
+
 import time
+from maze_support import *
 
+def iterations():
+    if sys.implementation.name == 'micropython':
+        return 100
+    else:
+        return 10000
 
+print(sys.implementation.name)
+print("start...")
 cost_lin = [0]*256
 cost_xy = [[0]*16]*16
 
@@ -16,14 +26,14 @@ def clear_xy_cost():
         for j in range(16):
             cost_xy[i][j] = 0 
 
-t = time.time()
-for i in range(10000):
+t = millis()
+for i in range(iterations()):
     clear_lin_cost()
-t = time.time() - t
-print(f"Linear: {t:.6f} seconds")
+t = millis() - t
+print(f"Linear: {t:} millisseconds")
 
-t = time.time() 
-for i in range(10000):
+t = millis() 
+for i in range(iterations()):
     clear_xy_cost()
-t = time.time() - t 
-print(f"XY: {t:.6f} seconds")            
+t = millis() - t 
+print(f"XY: {t:} milliseconds")            
