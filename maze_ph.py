323c59ca8906958ceaa025c62fd26bb9309d53e7
Peter Harrison <peter.harrison@helicron.net>
2024-10-23 22:33:10 +0100

    refactor maze_ph.py

--- maze_ph.py
+++ maze_ph.py
@@ -9,19 +9,18 @@ from maze_support import *
 import os
 import sys
 
-class Maze:
-    
 
+class Maze:
 
     def __init__(self, size=16):
         self.size = size
-        self.distances = [[None for _ in range(self.size)] for _ in range(self.size)]
+        self.cost = [[None for _ in range(
+            self.size)] for _ in range(self.size)]
         self.walls = {
             (row, col): {"N": WALL_UNKNOWN, "E": WALL_UNKNOWN, "S": WALL_UNKNOWN, "W": WALL_UNKNOWN}
             for row in range(size)
             for col in range(size)
         }
-
 
     def clear_walls(self):
         self.walls = {
@@ -52,15 +51,14 @@ class Maze:
         if (col, row) in self.walls and direction in self.walls[(col, row)]:
             return self.walls[(col, row)][direction]
         return
-    
+
     def has_wall(self, col, row, direction):
         if (col, row) in self.walls and direction in self.walls[(col, row)]:
-            return self.walls[(col,row)][direction] == WALL_PRESENT
+            return self.walls[(col, row)][direction] == WALL_PRESENT
         return False
 
     def print_maze(self):
         for row in range(self.size - 1, -1, -1):
-            # Print the north walls and top boundary
             line = "+"
             for col in range(self.size):
                 if self.get_wall(col, row, "N") == WALL_PRESENT:
@@ -68,25 +66,21 @@ class Maze:
                 else:
                     line += "   +"
             print(line)
-
-            # Print the west walls and cell boundary
             line = ""
             for col in range(self.size):
                 if self.get_wall(col, row, "W") == WALL_PRESENT:
                     line += "|"
                 else:
                     line += " "
-                if self.distances[row][col] is not None:
-                    line += f"{self.distances[col][row]:>3}"
+                if self.cost[row][col] is not None:
+                    line += f"{self.cost[col][row]:>3}"
                 else:
                     line += "   "
-            if self.get_wall(15, row, "E") == WALL_PRESENT: 
+            if self.get_wall(15, row, "E") == WALL_PRESENT:
                 line += "|"  # Rightmost boundary
             else:
-                line += " " 
+                line += " "
             print(line)
-
-        # Print the bottom boundary
         line = "+"
         for col in range(self.size):
             if self.get_wall(col, 0, "S") == WALL_PRESENT:
@@ -95,43 +89,39 @@ class Maze:
                 line += "   +"
         print(line)
 
-
     def flood(self, target_col, target_row):
-        frontier = [(0, 0, 0)] *100
+        self.cost = [[MAX_COST for _ in range(
+            self.size)] for _ in range(self.size)]
+        QUEUE_LENGTH = 64
+        queue = [(0, 0, 0)] * QUEUE_LENGTH
         tail = 0
         head = 0
-        frontier[tail] = (target_col, target_row, 0)
+        queue[tail] = (target_col, target_row, 0)
         tail += 1
         visited = set()
-
         while head != tail:
-            col, row, dist = frontier[head]
+            x, y, dist = queue[head]
             head += 1
-            if (head >= 100):
+            if (head >= QUEUE_LENGTH):
                 head = 0
+            for direction, (dx, dy) in {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}.items():
+                x_new = x + dx
+                y_new = y + dy
+                if not self.has_wall(x, y, direction) and self.cost[x_new][y_new] == MAX_COST:
+                    queue[tail] = (x_new, y_new, dist + 1)
+                    self.cost[x_new][y_new] = dist + 1
+                    tail += 1
+                    if (tail >= QUEUE_LENGTH):
+                        tail = 0
 
-            if (col,row) in visited:
-                continue
+        return self.cost
 
-            visited.add((col,row))
-            self.distances[col][row] = dist
-
-            for direction, (dc, dr) in {'N': (0, 1), 'E': (1, 0), 'S': (0,-1), 'W': (-1, 0)}.items():
-                new_row, new_col = row + dr, col + dc
-                if 0 <= new_row < self.size and 0 <= new_col < self.size:
-                    if not self.has_wall(col, row, direction) and (new_col, new_row) not in visited:
-                        frontier[tail]=(new_col, new_row, dist + 1)
-                        tail += 1
-                        if (tail >= 100):    
-                            tail = 0
-
-        return self.distances
 
 if __name__ == "__main__":
     # Example usage
     maze = Maze()
 
-    maze.clear_walls()   
+    maze.clear_walls()
     maze.set_wall(7, 7, "W", WALL_PRESENT)
     maze.set_wall(7, 7, "S", WALL_PRESENT)
     maze.set_wall(8, 7, "S", WALL_PRESENT)
@@ -147,12 +137,12 @@ if __name__ == "__main__":
     maze.set_wall(4, 4, "W", WALL_PRESENT)
 
     maze.print_maze()
-       
+
     start_time = millis()
     for _ in range(iterations()):
         distances = maze.flood(7, 7)  # Assuming the target cell is at (7, 7)
     end_time = millis()
     t = end_time - start_time
     maze.print_maze()
-    print("Flood distance correct: ",maze.distances[0][0] == 20)
+    print("Flood distance correct: ", maze.cost[0][0] == 20)
     print(f"{sys.implementation.name} - maze_ph: Execution Time for {iterations()} iterations: {t:} milliseconds")
