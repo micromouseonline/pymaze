22a96e4155bc238b5c628d63396d70e61bc3c17e
Peter Harrison <peter.harrison@helicron.net>
2024-10-25 23:58:55 +0100

    fixes issues in the maze_support.py file

--- maze_support.py
+++ maze_support.py
@@ -1,10 +1,10 @@
 # a place for most of the custom classes that are used
 
 
-
 import array
 import sys
 import time
+
 
 def millis():
     if sys.implementation.name == 'micropython':
@@ -13,22 +13,28 @@ def millis():
     else:
         # Desktop Python
         return int(round(time.time() * 1000))
-    
+
 
 def iterations():
     if sys.implementation.name == 'micropython':
         return 1
     else:
-        return 1000   
-    
+        return 1000
+
+
 MAZE_SIZE = 16
 MAZE_CELL_COUNT = MAZE_SIZE*MAZE_SIZE
-MAX_COST = 256
+MAX_COST = MAZE_CELL_COUNT
 
-WALL_UNKNOWN = 0
+WALL_ABSENT = 0
 WALL_PRESENT = 1
-WALL_ABSENT = 2
+WALL_UNKNOWN = 2
 WALL_VIRTUAL = 3
+WALL_MASK = 3
+
+
+CLOSED_MAZE_MASK = 3
+OPEN_MAZE_MASK = 1
 
 VIEW_PLAIN = 0
 VIEW_COSTS = 1
@@ -45,11 +51,6 @@ DIR_WEST = 3
 DIR_COUNT = 4
 DIR_BLOCKED = -1
 
-# bit masks used for wall presence
-NORTH_WALL = (1 << DIR_NORTH)
-EAST_WALL  = (1 << DIR_EAST)
-SOUTH_WALL = (1 << DIR_SOUTH)
-WEST_WALL  = (1 << DIR_WEST)
 
 """
  * Heading represents the direction that the robot is facing. 
@@ -62,20 +63,22 @@ HDG_WEST = 3
 HDG_COUNT = 4
 HDG_BLOCKED = -1
 
-def right_from(heading : int) -> int :
+
+def right_from(heading: int) -> int:
     return ((heading + 1) % HDG_COUNT)
 
 
-def left_from(heading : int) -> int :
+def left_from(heading: int) -> int:
     return ((heading + HDG_COUNT - 1) % HDG_COUNT)
 
 
-def ahead_from(heading : int) -> int :
+def ahead_from(heading: int) -> int:
     return heading
 
 
-def behind_from(heading : int) -> int :
+def behind_from(heading: int) -> int:
     return ((heading + 2) % HDG_COUNT)
+
 
 # '''
 #  * Directions are relative to the robot's orientation. They do not refer to
@@ -89,7 +92,6 @@ def behind_from(heading : int) -> int :
 #     BACK = 2
 #     LEFT = 3
 #     DIRECTION_COUNT = 4
- 
 
 '''
  * Location stores a position in the maze as an (x,y) coordinate pair(zero index). This is
@@ -106,11 +108,13 @@ def behind_from(heading : int) -> int :
  *     it is particularly irksome to pass the size all the time
 
  '''
-class Location :
-    #TODO this class has a big reliance on the overall maze size. Need to find a way to have this set once for each run rather than keep passing it around from the maze object
-    #TODO type hinting fails for methods that return an instance of theselves. 
- 
-    def __init__(self, x: int=0, y: int=0, size : int=16) :
+
+
+class Location:
+    # TODO this class has a big reliance on the overall maze size. Need to find a way to have this set once for each run rather than keep passing it around from the maze object
+    # TODO type hinting fails for methods that return an instance of theselves.
+
+    def __init__(self, x: int = 0, y: int = 0, size: int = 16):
         self.x = x
         self.y = y
         self.size = size
@@ -118,53 +122,46 @@ class Location :
     def __str__(self):
         return f"<Location({self.x},{self.y})>"
 
-    def is_in_maze(self) -> bool :
+    def is_in_maze(self) -> bool:
         return self.x < self.size and self.y < self.size
-    
-    
-    def operatorEqual(self, obj) -> bool :
-         return self.x == obj.x and self.y == obj.y
-    
 
-    def operatorNotEqual(self, obj) -> bool :
-         return self.x != obj.x or self.y != obj.y
-    
+    def operatorEqual(self, obj) -> bool:
+        return self.x == obj.x and self.y == obj.y
+
+    def operatorNotEqual(self, obj) -> bool:
+        return self.x != obj.x or self.y != obj.y
 
     # these operators prevent the user from exceeding the bounds of the maze
     # by wrapping to the opposite edge
-    def north(self) : #-> Location :
+    def north(self):  # -> Location :
         return Location(self.x, (self.y + 1) % self.size)
-    
 
-    def east(self) : #-> Location :
+    def east(self):  # -> Location :
         return Location((self.x + 1) % self.size, self.y)
-    
 
-    def south(self) : #-> Location :
+    def south(self):  # -> Location :
         return Location(self.x, (self.y + self.size - 1) % self.size)
-    
 
-    def west(self) : #-> Location :
+    def west(self):  # -> Location :
         return Location((self.x + self.size - 1) % self.size, self.y)
-    
 
-    def neighbour(self, heading : int)  -> 'Location'  : #-> Location :
-        
-        if heading == HDG_NORTH :
+    def neighbour(self, heading: int) -> 'Location':  # -> Location :
+
+        if heading == HDG_NORTH:
             return self.north()
-            
+
         elif heading == HDG_EAST:
             return self.east()
-            
+
         elif heading == HDG_SOUTH:
             return self.south()
-            
+
         elif heading == HDG_WEST:
             return self.west()
-            
-        else :
+
+        else:
             return Location()  # TODO this is actually an error and should be handled
-        
+
     # def toArray(self) -> array.array[int] :
     #     data = array.array('i', (0 for _ in range(4)))
     #     data[0] = self.x
@@ -172,16 +169,15 @@ class Location :
     #     data[2] = self.mazeWidth
     #     data[3] = self.size
 
-        return data            
+        return data
 
 
-
-class WallInfo :
+class WallInfo:
     def __init__(self):
         self.north = WALL_UNKNOWN
         self.east = WALL_UNKNOWN
         self.south = WALL_UNKNOWN
-        self.west = WALL_UNKNOWN     
+        self.west = WALL_UNKNOWN
 
     def __str__(self):
-        return f"<WallInfo({self.north},{self.east},{self.south},{self.west})>"       
+        return f"<WallInfo({self.north},{self.east},{self.south},{self.west})>"
