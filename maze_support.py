292719015c11a4a61520f2846c2a1665ab4dcc26
micromouseonline <peter.harrison@helicron.net>
2024-10-27 15:06:53 +0000

    add move types and move calculator

--- maze_support.py
+++ maze_support.py
@@ -1,7 +1,3 @@
-# a place for most of the custom classes that are used
-
-
-import array
 import sys
 import time
 
@@ -39,6 +35,21 @@ HDG_WEST = 3
 HDG_COUNT = 4
 HDG_BLOCKED = -1
 
+MOVE_AHEAD = 0
+MOVE_LEFT = 1
+MOVE_RIGHT = 2
+MOVE_BACK = 3
+MOVE_COUNT = 4
+
+def move_from_heading_change(heading_now, heading_next):
+    if heading_next == heading_now:
+        return MOVE_AHEAD
+    elif heading_next == left_from(heading_now):
+        return MOVE_LEFT
+    elif heading_next == right_from(heading_now):
+        return MOVE_RIGHT
+    else:
+        return MOVE_BACK
 
 def right_from(heading: int) -> int:
     return ((heading + 1) % HDG_COUNT)
@@ -54,3 +65,19 @@ def ahead_from(heading: int) -> int:
 
 def behind_from(heading: int) -> int:
     return ((heading + 2) % HDG_COUNT)
+
+if __name__ == "__main__":
+    print("test headings:")
+    heading_name = {HDG_NORTH : 'NORTH', HDG_SOUTH : 'SOUTH', HDG_WEST : 'WEST', HDG_EAST : 'EAST'}
+    move_name = {MOVE_AHEAD : 'AHEAD', MOVE_LEFT : 'LEFT', MOVE_RIGHT : 'RIGHT', MOVE_BACK : 'BACK'}
+    line = f"         "
+    for hdg in range(HDG_COUNT):
+        line += f"{heading_name[hdg]:8}"
+    print(line)
+
+    for now in range(HDG_COUNT):
+        line = f"{heading_name[now]:8} "
+        for next in range(HDG_COUNT):
+            line += f"{move_name[move_from_heading_change(now, next)]:8}"
+
+        print(line)
